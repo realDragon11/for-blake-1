38728a46f9fe9c790f76e16dfa7662c304454dcb
realDragon11 <43188351+realDragon11@users.noreply.github.com>
2020-09-02 17:57:13 -0400

    test

---
+++ Lab1 210/Assets/SomeFunnyScriptName.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SomeFunnyScriptName : MonoBehaviour
+{
+
+    Rigidbody obj;
+    // Start is called before the first frame update
+    void Start()
+    {
+        obj = this.gameObject.GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow)) {
+            obj.AddForce(Vector3.left * 20);
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            obj.AddForce(Vector3.right * 20);
+        }
+        if (this.gameObject.transform.position.y < -20) {
+            SceneManager.LoadScene(0);
+        }
+    }
+}
---
+++ Lab1 210/Assets/attacker_script.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class attacker_script : MonoBehaviour
+{
+
+    Rigidbody obj;
+    public GameObject player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        obj = this.gameObject.GetComponent<Rigidbody>();
+        obj.AddForce(new Vector3(0,0,-1) * 10000);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (this.transform.position.z < -10) {
+
+            this.transform.position = new Vector3(player.transform.position.x+Random.Range(-3f,3f),this.transform.position.y,10);
+        }
+    }
+
+    void FixedUpdate()
+    {
+
+        if (Mathf.Abs(obj.velocity.z) < 2) {
+            obj.AddForce(new Vector3(0, 0, -1) * 100);
+        }
+    }
+}
